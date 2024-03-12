c7d23f81e051a5f358f877263e8269044947958d
mohsen <amohsenmohamed671@gmail.com>
2024-03-12 14:07:16 +0200

    initial commit

---
+++ upward_contract_support/models/contract_support.py
@@ -0,0 +1,37 @@
+from odoo import models, fields,api
+from datetime import date, datetime, timedelta
+
+class ContractSupport(models.Model):
+    _name = 'contract.support'
+    _description = 'upward_contract_support'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    name = fields.Char(string='Name', required=True)
+    state = fields.Selection([('draft', 'Draft'),
+                              ('progress', 'In Progress'),
+                              ('cancel', 'Canceled ')], default='draft')
+    date_to = fields.Date(string='Date To',default=date.today())
+    date_from = fields.Date(string='Date From')
+    partner_id = fields.Many2one('res.partner', string='Partner')
+    contract_id = fields.Many2one('hr.contract', string='Contract')
+
+    def action_progress(self):
+        for rec in self:
+            rec.state = 'progress'
+
+
+    def action_cancel(self):
+        for rec in self:
+            rec.state = 'cancel'
+    @api.onchange('date_to')
+    def cancel_contract(self):
+        for rec in self:
+            state=rec.state
+            if rec.date_to > fields.Date.today():
+                rec.state = 'cancel'
+            else:
+                rec.state=state
+    def _test_cron(self):
+        for rec  in self:
+            print('fsad;jf;adsj')
+
